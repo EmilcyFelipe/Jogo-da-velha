9a469a077f14b93b4259256cba25b85945b39142
Felipe de Paula <emilcyfelipe@gmail.com>
2021-04-19 11:27:19 -0300

    Jogadas

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.addEventListener(click,function(){
-    var clickedPiece=document.getElementById
-})
+document.addEventListener('DOMContentLoaded',()=>{
+
+
+let piece = document.querySelectorAll('.piece');
+    piece.forEach((piece) => {
+    piece.addEventListener('click',handleClick);
+});
+
+});
+ let count=0;
+function handleClick(event){
+    count++;
+    if(count%2==0){
+        event.target.classList.add("show-face-shield");
+    }else{
+        event.target.classList.add("show-face-sword");
+
+}}
+
+
